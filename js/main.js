d2668a93e43bf33981c2fd867bfcf8112775b24d
Alessandro Oviszach <78302740+AlessandroOviszach@users.noreply.github.com>
2021-11-12 00:08:50 +0100

    inner img and title

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log('JS OK');
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        title : 'Contenuto da stampare interno a h1',
+        image : 'https://images.pexels.com/photos/7545780/pexels-photo-7545780.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+    }
+  })
